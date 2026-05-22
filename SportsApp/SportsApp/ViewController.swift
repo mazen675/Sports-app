bd74f9c5492157a8534684d973ef21b20027556f
Mazen Amr <139044003+mazen675@users.noreply.github.com>
2026-05-22 16:41:40 +0300

    Project Creation

---
+++ SportsApp/SportsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SportsApp
+//
+//  Created by Mazen Amr on 22/05/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
