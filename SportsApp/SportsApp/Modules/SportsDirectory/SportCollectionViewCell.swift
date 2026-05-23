7fc26cd9d21d719733fa87096744da06b222d32b
Mazen Amr <139044003+mazen675@users.noreply.github.com>
2026-05-23 19:03:17 +0300

    initial sports screen

---
+++ SportsApp/SportsApp/Modules/SportsDirectory/SportCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  SportCollectionViewCell.swift
+//  SportsApp
+//
+//  Created by Mazen Amr on 23/05/2026.
+//
+
+import UIKit
+
+class SportCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var sportNameLabel: UILabel!
+    @IBOutlet weak var sportImageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
